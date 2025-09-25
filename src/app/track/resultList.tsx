b42845d92b626900e3d0ce4c830f759576490c09
Garcat <gar@yeah.net>
2025-09-25 15:55:37 +1000

    update result with trackcNo.

--- src/app/track/resultList.tsx
+++ src/app/track/resultList.tsx
@@ -15,13 +15,17 @@ const ResultList = ({trackNo, results}: Props) => {
     return (
         <ul>
             {results.map((result, index) => {
-                const status = JSON.stringify(result.data.hisList[0].toStatus);
-                const createDate = JSON.stringify(result.data.hisList[0].createDate);
-                return (
-                    <li className="border border-gray-300 rounded p-2 m-2" key={index}>
-                        {`${trackNo[index]}:   ${status} / ${createDate}`}
-                    </li>
-                );
+                // Only display if we have a valid result and corresponding trackNo
+                if (result && result.data && result.data.hisList && result.data.hisList.length > 0 && index < trackNo.length) {
+                    const status = JSON.stringify(result.data.hisList[0].toStatus);
+                    const createDate = JSON.stringify(result.data.hisList[0].createDate);
+                    return (
+                        <li className="border border-gray-300 rounded p-2 m-2" key={index}>
+                            {`${trackNo[index]}:   ${status} / ${createDate}`}
+                        </li>
+                    );
+                }
+                return null;
             })}
         </ul>
     )
